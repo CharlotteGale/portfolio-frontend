1bcdaa94597664193f05249e295f2801d12aa975
Charlotte Gale <CharlotteMGale@outlook.com>
2025-02-17 09:49:00 +0000

    amend Navbar.jsx to use <Link /> rather than <a> for navigation

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
 import '../assets/styles/components/Navbar.css';
 
 const Navbar = () => {
     return (
         <nav className="navbar">
             <ul>
-                <li><a href="#about">About</a></li>
-                <li><a href="#projects">Projects</a></li>
-                <li><a href="#languages">Languages</a></li>
-                <li><a href="#tools&tech">Tools</a></li>
-                <li><a href="#contact">Contact</a></li>
+                <li><Link to="/">Home</Link></li>
+                <li><Link to="/about">About</Link></li>
+                <li><Link to="/projects">Projects</Link></li>
+                <li><Link to="/contact">Contact</Link></li>
             </ul>
         </nav>
     );
