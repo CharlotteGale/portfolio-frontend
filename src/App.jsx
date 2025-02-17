a0e0d04ce445d4c2a54dca8518c6307c56591d20
Charlotte Gale <CharlotteMGale@outlook.com>
2025-02-17 12:12:17 +0000

    begin amending styling for components and use components in relevant pages

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,10 @@ import Home from './pages/Home.jsx';
 import About from './pages/About.jsx';
 import Projects from './pages/Projects.jsx';
 import Contact from './pages/Contact.jsx';
+import Footer from './components/Footer.jsx';
 
 import './assets/styles/index.css';
 import './assets/styles/App.css';
-
 
 const App = () => {
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,11 +3,11 @@ import React from 'react';
 import Header from '../components/Header.jsx';
 import BannerImage from '../components/BannerImage.jsx';
 
-import '../assets/styles/components/Home.css';
+import '../assets/styles/pages/Home.css';
 
 const Home = () => {
     return (
-        <div>
+        <div className='home-container'>
             <Header />
             <BannerImage />
         </div>
