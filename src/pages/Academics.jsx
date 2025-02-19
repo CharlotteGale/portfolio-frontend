3c0016963490bfd92bc48e2d883f8fc2a33ecf02
Charlotte Gale <CharlotteMGale@outlook.com>
2025-02-19 18:45:59 +0000

    style Academics.jsx and reduce size of .cards in Certifications.jsx

--- src/pages/Academics.jsx
+++ src/pages/Academics.jsx
@@ -6,10 +6,10 @@ import '../assets/styles/pages/Academics.css'
 const Academics = () => {
     return (
         <section id="academics">
-            <h2>My Academic Achievements</h2>
+            <h1>My Academic Achievements</h1>
             <Certifications />
 
-            <h3>What I'm Learning</h3>
+            <h2>What I'm Learning</h2>
             <ProgressTracker />
         </section>
     );
