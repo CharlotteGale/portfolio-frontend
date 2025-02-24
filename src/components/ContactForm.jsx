cea6f0ab8d62f2e3b3fd34cd2bd37ce3c9fe964f
Charlotte Gale <CharlotteMGale@outlook.com>
2025-02-24 16:46:34 +0000

    set frontend to call API

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -1,10 +1,10 @@
-import React, { useState } from 'react';
-import emailjs from '@emailjs/browser'
+import React, { useEffect, useState } from 'react';
+// import emailjs from '@emailjs/browser';
 
 import "../assets/styles/components/ContactForm.css";
 
 const ContactForm = () => {
-  const [isDisabled, setIsDisabled] = useState(false);
+  const [isDisabled, setIsDisabled] = useState(true);
   const [formData, setFormData] = useState({
     name: "",
     email: "",
@@ -24,7 +24,7 @@ const ContactForm = () => {
     setIsLoading(true);
 
     try {
-
+      /*
       const serviceId = import.meta.env.VITE_EMAILJS_SERVICE_ID;
       const templateId = import.meta.env.VITE_EMAILJS_TEMPLATE_ID;
       const userId = import.meta.env.VITE_EMAILJS_PUBLIC_KEY;
@@ -43,9 +43,8 @@ const ContactForm = () => {
         console.error("Server response:", response);
         alert(`Error: ${response.text || "Failed to send message."}`)
       }
+      */
 
-
-      /*
       // const proxyUrl = "https://thingproxy.freeboard.io/fetch/"; // local testing only
       const apiUrl = "https://portfolio-backend-htjt.onrender.com/api/contact"
       const response = await fetch(apiUrl, {
@@ -63,7 +62,6 @@ const ContactForm = () => {
         console.error("Server response:", data);
         alert(`Error: ${data.message || "Failed to send message."}`);
       }
-    */
     
     } catch (error) {
       console.error("Error sending message:", error);
@@ -73,7 +71,36 @@ const ContactForm = () => {
     setIsLoading(false);
   };
 
+  const handleOAuth2Callback = async () => {
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+    const code = urlParams.get('code');
 
+    if (code) {
+      try {
+        const apiUrl = "https://portfolio-backend-htjt.onrender.com/api/contact/oauth2callback";
+        const response = await fetch(`${apiUrl}?code=${code}`, {
+          method: 'GET',
+        });
+
+        const data = await response.json();
+
+        if (response.ok) {
+          alert('OAuth authentication successful!');
+        } else {
+          console.error('Server response:', data);
+          alert(`Error: ${data.message || 'OAuth2 authentication failed.'}`);
+        }
+      } catch (error) {
+        console.error('Error during OAuth2 callback:', error);
+        alert('There was a problem during OAuth2 authentication.');
+      }
+    }
+  };
+
+  useEffect(() => {
+    handleOAuth2Callback();
+  }, []);
 
   return (
     <>
