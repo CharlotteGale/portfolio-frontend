e79c0a8fa898d67ad24542fa33b513f192f7a8fb
Charlotte Gale <CharlotteMGale@outlook.com>
2025-04-25 16:15:08 +0100

    amend routing in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import Navbar from './components/Navbar.jsx';
 import Home from './pages/Home.jsx';
@@ -17,22 +17,24 @@ import './assets/styles/App.css';
 const App = () => {
 
   return (
-    <div className='app-container'>
-      <Navbar />
-      <main className='main-content'>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path='/about' element={<About />} />
-          <Route path='/projects' element={<Projects />} />
-          <Route path='/skills' element={<Skills />} />
-          <Route path='/academics' element={<Academics />} />
-          <Route path='/contact' element={<Contact />} />
-          <Route path='/privacy-policy' element={<PrivacyPolicy />} />
-          <Route path='/terms-of-service' element={<TermsOfService />} />
-        </Routes>
-        <Footer />
-      </main>
-    </div>
+    <BrowserRouter>
+      <div className='app-container'>
+        <Navbar />
+        <main className='main-content'>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path='/about' element={<About />} />
+            <Route path='/projects' element={<Projects />} />
+            <Route path='/skills' element={<Skills />} />
+            <Route path='/academics' element={<Academics />} />
+            <Route path='/contact' element={<Contact />} />
+            <Route path='/privacy-policy' element={<PrivacyPolicy />} />
+            <Route path='/terms-of-service' element={<TermsOfService />} />
+          </Routes>
+          <Footer />
+        </main>
+      </div>
+    </BrowserRouter>
   )
 }
 
